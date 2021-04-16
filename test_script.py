79238c1d174e3b1a46260b902a7ff6fb22ecfc21
gooner132 <gooner132@hotmail.com>
2021-04-16 12:28:01 +0200

    Update test_script.py

--- test_script.py
+++ test_script.py
@@ -1,12 +1,13 @@
 import time
 
-
-print ("Script 'test_script.py is starting !!")
-print ("Sleeping for " str(sleep_time) + " seconds!", flush=True)
 sleep_time = 15
+print ("Script 'test_script.py is starting !!")
+print ("Sleeping for " + str (sleep_time) + " seconds!", flush=True)
 
+time.sleep(sleep_time)
 
 print ("I'm back!!!", flush=True)
 
 
 # EOF #
+
