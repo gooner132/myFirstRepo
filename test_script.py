f19ad5336121fbcd41232b3f5f854a0c1df5855d
gooner132 <gooner132@hotmail.com>
2021-04-16 14:22:22 +0200

    Update test_script.py

--- test_script.py
+++ test_script.py
@@ -1,13 +1,14 @@
-import time
+import time, sys
 
-sleep_time = 15
-print ("Script 'test_script.py is starting !!")
-print ("Sleeping for " + str (sleep_time) + " seconds!", flush=True)
+sleep_time = 3
+print("Script 'test_script.py is starting !!")
+print("Sleeping for " + str(sleep_time) + " seconds!", flush=True)
 
 time.sleep(sleep_time)
 
-print ("I'm back!!!", flush=True)
+print("I'm back!!!", flush=True)
 
+print('Number of arguments:', len(sys.argv), 'arguments.')
+print('Argument List:', str(sys.argv))
 
 # EOF #
-
