d0ece95cbd08c3f3b89c9f4bd7e5fee2a3c5639f
gooner132 <gooner132@hotmail.com>
2021-04-19 19:46:31 +0200

    Update test_script2.py

--- test_script2.py
+++ test_script2.py
@@ -3,6 +3,14 @@
 f = open("what.txt", "r")
 print(f.read())
 
+test_str = f.read()
+new_str = ""
+
+for i in test_str:
+    new_str = new_str + i + "x"
+
+print(new_str)
+
 # sleep_time = 3
 # print("Script 'test_script.py is starting !!")
 # print("Sleeping for " + str(sleep_time) + " seconds!", flush=True)
