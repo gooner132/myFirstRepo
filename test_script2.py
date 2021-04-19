88595a0782fe87c4557945f8b217b4a4c9fde439
gooner132 <gooner132@hotmail.com>
2021-04-19 19:36:00 +0200

    Create test_script2.py

---
+++ test_script2.py
@@ -0,0 +1,23 @@
+import time, sys
+
+print ()
+
+f = open("what.txt", "a")
+f.write("Now the file has more content!")
+f.close()
+
+f = open("what.txt", "r")
+print(f.read())
+
+# sleep_time = 3
+# print("Script 'test_script.py is starting !!")
+# print("Sleeping for " + str(sleep_time) + " seconds!", flush=True)
+#
+# time.sleep(sleep_time)
+#
+# print("I'm back!!!", flush=True)
+#
+print('Number of arguments:', len(sys.argv), 'arguments.')
+print('Argument List:', str(sys.argv))
+
+# EOF #
