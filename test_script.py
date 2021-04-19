655c3b009329f0b8aa5c93ffcba901887ac43115
gooner132 <gooner132@hotmail.com>
2021-04-19 18:47:58 +0200

    Update test_script.py

--- test_script.py
+++ test_script.py
@@ -1,23 +1,20 @@
-import time, sys
+# import time, sys
 
-sleep_time = 3
-print("Script 'test_script.py is starting !!")
-print("Sleeping for " + str(sleep_time) + " seconds!", flush=True)
+print ()
 
-time.sleep(sleep_time)
+f = open("what.txt", "r")
+print(f.read())
 
-print("I'm back!!!", flush=True)
-
-print('Number of arguments:', len(sys.argv), 'arguments.')
-print('Argument List:', str(sys.argv))
-
-test_string = sys.argv[1]
-print(test_string)
-
-new_str = ""
-for x in test_string:
-    new_str = new_str + x + "x"
-
-print(new_str)
+# sleep_time = 3
+# print("Script 'test_script.py is starting !!")
+# print("Sleeping for " + str(sleep_time) + " seconds!", flush=True)
+#
+# time.sleep(sleep_time)
+#
+# print("I'm back!!!", flush=True)
+#
+# print('Number of arguments:', len(sys.argv), 'arguments.')
+# print('Argument List:', str(sys.argv))
 
 # EOF #
+
